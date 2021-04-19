3a0c0849a1b1b797b1697e51f603267d4d486312
Ruman-Islam <rumanislam48@gmail.com>
2021-04-19 12:35:44 +0600

    fixed

--- src/components/Home/Navbar/Navbar.js
+++ src/components/Home/Navbar/Navbar.js
@@ -6,7 +6,7 @@ const Navbar = () => {
     const [loggedInUser] = useContext(userContext);
     return (
         <nav class="navbar navbar-expand-lg navbar-light container">
-            <a class="navbar-brand" href="#"><span className="text-secondary">Medicine</span><span className="text-primary">Plus</span></a>
+            <a style={{fontSize: '40px'}} class="navbar-brand" href="#"><span className="text-secondary">Medicine</span><span className="text-primary">Plus</span></a>
             <div class="container-fluid">
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
@@ -14,22 +14,22 @@ const Navbar = () => {
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
                         <li class="nav-item">
-                            <Link className="ms-3 nav-link" aria-current="page" to="/">Home</Link>
+                            <Link style={{fontSize: '20px'}} className="ms-3 nav-link" aria-current="page" to="/">Home</Link>
                         </li>
                         <li class="nav-item">
-                            <Link className="ms-3 nav-link" to="/services">Medical Services</Link>
+                            <Link style={{fontSize: '20px'}} className="ms-3 nav-link" to="/services">Medical Services</Link>
                         </li>
                         <li class="nav-item">
-                            <Link className="ms-3 nav-link" to="/orders">Orders</Link>
+                            <Link style={{fontSize: '20px'}} className="ms-3 nav-link" to="/orders">Orders</Link>
                         </li>
                         <li class="nav-item">
-                            <Link className="ms-3 nav-link" to="/reviews">Reviews</Link>
+                            <Link style={{fontSize: '20px'}} className="ms-3 nav-link" to="/reviews">Reviews</Link>
                         </li>
                         <li class="nav-item">
-                            <Link className="ms-3 nav-link" to="/admin">Admin</Link>
+                            <Link style={{fontSize: '20px'}} className="ms-3 nav-link" to="/admin">Admin</Link>
                         </li>
                         <li class="nav-item">
-                            <Link className="ms-3 nav-link" to="/login">{loggedInUser.name ? loggedInUser.name : 'Login'}</Link>
+                            <Link style={{fontSize: '20px'}} className="ms-3 nav-link" to="/login">{loggedInUser.name ? loggedInUser.name : 'Login'}</Link>
                         </li>
                     </ul>
                 </div>
